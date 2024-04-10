29180b85bc04eef36212a33f0a64d110d69016bb
mx-smblt <mx.smblt@gmail.com>
2024-04-10 13:40:58 +0300

    Added the new module `airflux-commons-collections`.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,6 @@ project(":airflux-commons-text").projectDir = file("./modules/text")
 
 include(":airflux-commons-kotest-assertions")
 project(":airflux-commons-kotest-assertions").projectDir = file("./modules/kotest/assertions")
+
+include(":airflux-commons-collections")
+project(":airflux-commons-collections").projectDir = file("./modules/collections")
