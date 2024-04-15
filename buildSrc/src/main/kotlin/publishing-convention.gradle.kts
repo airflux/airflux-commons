c0a2beb330603d245755cffddce980c2068b4dd7
mx-smblt <mx.smblt@gmail.com>
2024-04-15 16:25:18 +0300

    Changed the Gradle configuration script.

--- buildSrc/src/main/kotlin/publishing-convention.gradle.kts
+++ buildSrc/src/main/kotlin/publishing-convention.gradle.kts
@@ -7,7 +7,8 @@ configure<SigningExtension> {
     val signingKey: String? = System.getenv("GPG_PRIVATE_KEY")
     val signingKeyPassphrase: String? = System.getenv("GPG_PRIVATE_PASSWORD")
     setRequired {
-        !signingKey.isNullOrBlank()
+        val required = !signingKey.isNullOrBlank()
+        println("Task is required: " + required)
     }
     useInMemoryPgpKeys(signingKey, signingKeyPassphrase)
 }
