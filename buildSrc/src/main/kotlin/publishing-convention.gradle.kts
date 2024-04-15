8e96bc71cd5ed3f34b7c070e026262ff115f8155
mx-smblt <mx.smblt@gmail.com>
2024-04-15 20:18:05 +0300

    Changed the Gradle configuration script.

--- buildSrc/src/main/kotlin/publishing-convention.gradle.kts
+++ buildSrc/src/main/kotlin/publishing-convention.gradle.kts
@@ -8,3 +8,7 @@ configure<SigningExtension> {
     val signingKeyPassphrase: String? = System.getenv("GPG_PRIVATE_PASSWORD")
     useInMemoryPgpKeys(signingKey, signingKeyPassphrase)
 }
+
+tasks.withType<Sign>() {
+    onlyIf("this is a release build") { isReleaseBuild() }
+}
